76c1d181b0bb11b8a1a5f516b8fd35ce82e08644
madson7s <madsonsilva909@gmail.com>
2020-02-11 19:31:54 -0300

    Projeto concluido com todos os testes feitos conforme ccada regra

--- src/test/java/br/edu/ifal/TrianguloTest.java
+++ src/test/java/br/edu/ifal/TrianguloTest.java
@@ -1,8 +1,8 @@
+
 package br.edu.ifal;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -13,31 +13,7 @@ public class TrianguloTest {
     Triangulo triangulo;
 
     
-    @Before
-    public void inicializacao(){
-
-    }
-    @Test
-    public void deveRetornarIlegalParaLadosIguaisAZero() {
-        triangulo = new Triangulo(0, 0, 0);
-        final String valorRetornado = triangulo.getTipo(triangulo);
-        final String valorEsperado = "Ilegal";
-        assertEquals(valorEsperado, valorRetornado);
-    }
-    @Test
-    public void deveRetornarIlegalParaUmDosLadosIgualAZero() {
-        triangulo = new Triangulo(0, 1, 2);
-        final String valorRetornado = triangulo.getTipo(triangulo);
-        final String valorEsperado = "Ilegal";
-        assertEquals(valorEsperado, valorRetornado);
-    }@Test
-    public void deveRetornarIlegalSeDoisDosTresLadosForemIgualAZero() {
-        triangulo = new Triangulo(0, 0, 1);
-        final String valorRetornado = triangulo.getTipo(triangulo);
-        final String valorEsperado = "Ilegal";
-        assertEquals(valorEsperado, valorRetornado);
-    }
-
+   
     @Test
     public void deveRetornarEscalenoParaTodosOsLadosDiferentes() {
         triangulo = new Triangulo(2, 3, 4);
@@ -76,5 +52,58 @@ public class TrianguloTest {
         assertEquals(valorEsperado, valorRetornado);
 
     }
+    @Test
+    public void deveRetornarIlegalParaLadosIguaisAZero() {
+        triangulo = new Triangulo(0, 0, 0);
+         String valorRetornado = triangulo.getTipo(triangulo);
+         String valorEsperado = "Ilegal";
+        assertEquals(valorEsperado, valorRetornado);
+    }
+    @Test
+    public void deveRetornarIlegalParaUmDosLadosIgualAZero() {
+        triangulo = new Triangulo(0, 1, 2);
+         String valorRetornado = triangulo.getTipo(triangulo);
+         String valorEsperado = "Ilegal";
+        assertEquals(valorEsperado, valorRetornado);
+    }@Test
+    public void deveRetornarIlegalSeDoisDosTresLadosForemIgualAZero() {
+        triangulo = new Triangulo(0, 0, 1);
+         String valorRetornado = triangulo.getTipo(triangulo);
+         String valorEsperado = "Ilegal";
+        assertEquals(valorEsperado, valorRetornado);
+    }
+@Test
+public void deveRetornarIlegalQuandoOLadoCForMenorQueASubtracaoDoLadoAComOLadoB(){
+    triangulo = new Triangulo(8, 4, 2);
+     String valorRetornado = triangulo.getTipo(triangulo);
+     String valorEsperado = "Ilegal";
+    assertEquals(valorEsperado, valorRetornado);
+    
+}
+@Test
+public void deveRetornarIlegalQuandoOLadoAForMenorQueASubtracaoDoLadoBComOLadoC(){
+    triangulo = new Triangulo(2, 4, 8);
+     String valorRetornado = triangulo.getTipo(triangulo);
+     String valorEsperado = "Ilegal";
+    assertEquals(valorEsperado, valorRetornado);
+    
+}
+
+@Test
+public void deveRetornarIlegalQuandoOLadoBForMenorQueASubtracaoDoLadoAComOLadoC(){
+    triangulo = new Triangulo(4, 2, 8);
+     String valorRetornado = triangulo.getTipo(triangulo);
+     String valorEsperado = "Ilegal";
+    assertEquals(valorEsperado, valorRetornado);
+    
+}
+    @Test
+    public void deveRetornarIlegalQuandoASubtracaoDosDoisLadosForIgualADoOutroLado(){
+        triangulo = new Triangulo(5, 3, 8);
+         String valorRetornado = triangulo.getTipo(triangulo);
+         String valorEsperado = "Ilegal";
+        assertEquals(valorEsperado, valorRetornado);
+        
+}
 
 }
